06609ed963b36de82ac6751f40742f88f5b55f67
sagewall <sagemaps@gmail.com>
2018-07-31 15:46:20 -0600

    Add properties and template to detail component

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Address } from '../address';
+import esri = __esri;
 
 @Component({
   selector: 'app-detail',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.sass']
 })
 export class DetailComponent implements OnInit {
+
+  private _feature: esri.Graphic | Address;
+
+  @Input()
+  set feature(feature: esri.Graphic | Address) {
+    this._feature = feature;
+  }
+
+  get feature(): esri.Graphic | Address {
+    return this._feature;
+  }
 
   constructor() { }
 
